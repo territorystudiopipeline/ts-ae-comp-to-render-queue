e05ab4f7e26002f31ee13f29d3c9c7cea60fbd75
Ryan Lloyd <ryan.lloyd@territorystudio.com>
2024-06-13 14:41:09 +0200

    - Fixed Queuing Time Span / Duration Issues for using comp work area or custom frame range - Fixed Output path to include sub folder for comp name - Refactored calls that only need to happen once - Added More Alert Dialog types that cover the messaging more appropriately - Added More Debug logging info

--- python/app/dialog.py
+++ python/app/dialog.py
@@ -140,26 +140,42 @@ class AppDialog(QtGui.QWidget):
         """
             Create a render queue item for each of the selected comps
         """
+        # Get the selected comps
         selected_comps = self.get_selected_comps()
+        logger.debug("Selected comps: %s" % selected_comps)
+        logger.debug("Selected comps: %s" % len(selected_comps))
 
+        # Check if any comps are selected
         if len(selected_comps) == 0:
-            self.alert_box("No comps selected")
+            self.alert_box("No comps selected", "Please select one or more comps to add to the render queue")
+            return
 
         count = 0
+        render_queue_template = self.get_render_queue_template()
+        if render_queue_template is None:
+            self.alert_box("Error", "Failed to find selected render preset")
+            return
+        # Suppress dialogs
+        self.adobe.app.beginSuppressDialogs()
+
         for comp in selected_comps:
+
+            # Get the frame range to render
             frame_range = self.get_frame_range(comp)
             if frame_range[0] is None or frame_range[1] is None:
-                self.alert_box("Bad frame range")
+                logger.debug("Bad frame range, skipping %s" % comp.name)
+                self.alert_box("Bad frame range", "Please check the frame range for %s, Skipping" % comp.name)
+                pass
 
-            render_queue_template = self.get_render_queue_template()
-            if render_queue_template is None:
-                self.alert_box("Failed to find selected render preset")
-
+            # Create a render queue item for each of the selected comps
             self.create_render_queue_item_for_comp(comp, frame_range, render_queue_template)
             count += 1
 
+        # End Suppress Dialogs
+        self.adobe.app.endSuppressDialogs()
+
+        self.message_box( 'Add Comps To Render Queue', 'Successfully Added %d comps to the render queue' % count)
         self.close()
-        self.alert_box('Successfully created ' + str(count) + ' item(s) in the render queue')
 
     def get_frame_range(self, comp):
         """
@@ -172,17 +188,70 @@ class AppDialog(QtGui.QWidget):
         startFrame = None
         endFrame = None
 
+        # Debug Info Report for Comp
+        try:
+            logger.debug("*" * 50)
+            logger.debug(" Debug Info Report for Comp")
+            logger.debug("*" * 50)
+            logger.debug("Comp Name: %s" % comp.name)
+            logger.debug("Comp Frame Rate: %s" % comp.frameRate)
+            logger.debug("Comp Frame Duration: %s" % comp.frameDuration)
+            logger.debug("Comp Display Start Frame: %s" % comp.displayStartFrame)
+            logger.debug("Comp Display Start Time: %s" % comp.displayStartTime)
+            logger.debug("Comp Duration: %s" % comp.duration)
+            logger.debug("Comp Work Area Start: %s" % comp.workAreaStart)
+            logger.debug("Comp Work Area Duration: %s" % comp.workAreaDuration)
+            logger.debug("*" * 50)
+        except Exception as e:
+            logger.debug("Failed to get debug info for comp: %s" % e)
+
         # Use comp frame range
         if self.ui.frameRangeComboBox.currentText() == self.COMP_TEXT:
-            startFrame = comp.displayStartFrame
-            endFrame = int(comp.frameRate * comp.duration + 0.0001)
+            logger.debug("Using comp frame range")
+            # Get the work area start and end times
+            startFrame = comp.workAreaStart
+            endFrame = (startFrame + comp.duration)
+
+            # Debugging info
+            logger.debug("Start Time: %s" % startFrame)
+            logger.debug("End Time: %s" % endFrame)
+
+            # Convert to frame numbers
+            # Check if the comp work area has a start frame of 0
+            logger.debug("Checking if start time is 0")
+            if int(startFrame) == 0:
+                startFrameNum = comp.displayStartFrame
+
+            else:
+                startFrameNum = int((startFrame / comp.frameDuration)) + comp.displayStartFrame
+
+            # Start frame
+            logger.debug("Start Frame: %s" % startFrameNum)
+
+            # End frame
+            endFrameNum = int((endFrame / comp.frameDuration)) + comp.displayStartFrame
+            logger.debug("End Frame: %s" % endFrameNum)
+
+            #endFrame = int(comp.frameRate * comp.workAreaDuration)
+            #endFrame = int(comp.frameRate * comp.duration + 0.0001)
         else:
             rawText = self.ui.frameRangeLineEdit.text()
             # Assumed pattern is {Digits}{NonDigitSeperator}{Digits} - e.g. 1001-1002
             match = re.match(r'(\d+)(\D+)(\d+)', rawText)
+            logger.debug("Using custom frame range: %s" % rawText)
             if match:
                 startFrame = match.group(1)
                 endFrame = match.group(3)
+
+                # Change frame number to Time and calculate the start and end time durations according to the start time
+                logger.debug("Start Frame: %s" % startFrame)
+                startFrame = (comp.frameDuration * int(startFrame)) - comp.displayStartTime
+                logger.debug("Start Time: %s" % startFrame)
+
+                logger.debug("End Frame: %s" % endFrame)
+                endFrame = (comp.frameDuration * int(endFrame)) - comp.displayStartTime + 0.0001 # Add a small amount to ensure the last frame is included to avoid rounding errors
+                logger.debug("End Time: %s" % endFrame)
+
 
         return [startFrame, endFrame]
 
@@ -200,10 +269,50 @@ class AppDialog(QtGui.QWidget):
 
         return render_queue_template
 
-    def alert_box(self, text):
+    def alert_box(self, title, text):
+        """
+            Display an alert box
+
+            :param title: The title of the alert box
+            :param text: The text of the alert box
+        """
         QtGui.QMessageBox.critical(
             self,
-            "!",
+            title,
+            str(text),
+            buttons=QtGui.QMessageBox.Ok,
+            defaultButton=QtGui.QMessageBox.Ok,
+        )
+
+    def warning_box(self, title, text):
+        """
+            Display a warning box
+
+            :param title: The title of the warning box
+            :param text: The text of the warning box
+        """
+        logger.debug("Displaying Warning Box: %s" % text)
+        # Display the warning box
+        QtGui.QMessageBox.warning(
+            self,
+            title,
+            str(text),
+            buttons=QtGui.QMessageBox.Ok,
+            defaultButton=QtGui.QMessageBox.Ok,
+        )
+
+    def message_box(self, title, text):
+        """
+            Display a message box
+
+            :param title: The title of the message box
+            :param text: The text of the message box
+        """
+        logger.debug("Displaying Message Box: %s" % text)
+        # Display the message box
+        QtGui.QMessageBox.information(
+            self,
+            title,
             str(text),
             buttons=QtGui.QMessageBox.Ok,
             defaultButton=QtGui.QMessageBox.Ok,
@@ -221,19 +330,23 @@ class AppDialog(QtGui.QWidget):
 
         # Check the template actually exists
         if not self.check_template_exists(comp, frame_range, render_queue_template, templateName):
-            self.alert_box("Something went wrong applying or locating an output template")
+            self.alert_box("Error", "Something went wrong applying or locating an output template")
 
         renderQueueItem = self.adobe.app.project.renderQueue.items.add(comp)
         try:
             renderQueueItem.outputModule(renderQueueItem.numOutputModules).applyTemplate(templateName)
         except:
-            self.alert_box("There's some kind of issue with this template\n\n" + str(templateName) + '\n' + str(render_queue_template))
+            self.alert_box("Error", "There's some kind of issue with this template\n\n" + str(templateName) + '\n' + str(render_queue_template))
             return
 
         # Set the render to the start/end times
-        self.adobe.app.beginSuppressDialogs()
-        renderQueueItem.timeSpanStart = frame_range[0]
-        renderQueueItem.timeSpanDuration = comp.duration
+        #self.adobe.app.beginSuppressDialogs()
+        # Only set the time span if the frame range is not set to the comp frame range
+        # This will automatically set the time span to the comp duration
+        if self.ui.frameRangeComboBox.currentText() != self.COMP_TEXT:
+            renderQueueItem.timeSpanStart = frame_range[0]
+            renderQueueItem.timeSpanDuration = frame_range[1] - frame_range[0]
+        #renderQueueItem.timeSpanDuration = comp.duration
 
         # Grab the output folder from templates
         outputLocation = self.get_shotgrid_template(render_queue_template)
@@ -276,14 +389,22 @@ class AppDialog(QtGui.QWidget):
             logger.debug("New Output File: %s" % newOutputFile)
 
             # Rebuild the output location
-            outputLocation = os.path.join(folderPath, newOutputFile)
+            outputLocation = os.path.join(folderPath, compName, newOutputFile)
+            logger.debug("Output location: %s" % outputLocation)
+
+            # Create the output folder if it doesn't already exist
+            folderPath = os.path.dirname(outputLocation)
+            if not os.path.exists(folderPath):
+                os.makedirs(folderPath)
 
         # Set the filepath and name on the newly created output module
         # Do it twice because it sometimes fails the first time - Sean
         renderQueueItem.outputModule(renderQueueItem.numOutputModules).file = self.adobe.File(outputLocation)
         renderQueueItem.outputModule(renderQueueItem.numOutputModules).file = self.adobe.File(outputLocation)
 
-        self.adobe.app.endSuppressDialogs(alert=False)
+        # Log
+        logger.debug("Comp: %s has been added to the render queue" % comp.name)
+        #self.adobe.app.endSuppressDialogs(alert=False)
 
     def get_shotgrid_template(self, render_queue_template):
         """
